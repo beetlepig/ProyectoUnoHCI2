5f1ef386c830201ff5745dd27d3445cbb015146b
karlos <sky_karlos@hotmail.com>
2017-02-12 22:04:59 -0500

    Base

---
+++ HCI2ProyectoUno/src/main/java/Cliente/App.java
@@ -0,0 +1,12 @@
+package Cliente;
+
+import processing.core.PApplet;
+
+public class App extends PApplet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
