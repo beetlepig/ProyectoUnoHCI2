b635ee2ac3a30e570daf122cded3263798e689f7
webprogrammer <webprogrammer@SC203e27.academico.icesi.edu.co>
2017-03-06 15:38:51 -0500

    IDK!

--- HCI2ProyectoUno/src/main/java/Cliente/ComunicacionCliente.java
+++ HCI2ProyectoUno/src/main/java/Cliente/ComunicacionCliente.java
@@ -73,7 +73,7 @@ public class ComunicacionCliente extends Observable{
 				while (true) {
 					recibir();
 					try {
-						Thread.sleep(20);
+						Thread.sleep(5);
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
